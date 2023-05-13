80fee78fc1ce35a05eafff5b57dc9babc47f6f00
BrigthArias <116394234+BrigthArias@users.noreply.github.com>
2023-05-13 13:52:40 +0200

    Update 1- Song Analysis.py

--- pages/1- Song Analysis.py
+++ pages/1- Song Analysis.py
@@ -82,9 +82,9 @@ if selected_song and selected_artist:
     def returncolname(row, colnames):
         return colnames[np.argmax(row.values)]
     
-    topic_song['colmax'] = topic_song.apply(lambda x: returncolname(x, topic_song.columns), axis=1)
+    topic_song.loc[:,'colmax'] = topic_song.apply(lambda x: returncolname(x, topic_song.columns), axis=1)
     name_topic = topic_song['colmax'].values[0]
-    topic_song['max_topic'] = ((topic_song.max(axis=1))*100).round(2).astype(str) + '%'
+    topic_song.loc[:, 'max_topic'] = ((topic_song.max(axis=1))*100).round(2).astype(str) + '%'
     percentage_topic = topic_song['max_topic'].values[0]
     
            
